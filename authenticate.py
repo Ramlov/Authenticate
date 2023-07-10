f7e857a4b29abf24377f84a047e469418c8e8de9
Ramlov <mathias.ramlov10@gmail.com>
2023-07-10 19:35:58 +0200

    Made custom user agent + implemented discord webhook + blacklisted ip

--- authenticate.py
+++ authenticate.py
@@ -1,16 +1,44 @@
 import requests
+from json import dumps
 
 def auth(provided_string):
-    url = "https://website.website/"
+    url = ""
+    user_agent = "Custom User Agent"
+    webhookurl = ""
+
+    headers = {
+        "User-Agent": user_agent
+    }
 
     params = {'string': provided_string}
 
-    response = requests.get(url, params=params)
+    response = requests.get(url, headers=headers, params=params)
     result = response.text
 
-    if result.startswith('Ok - Normal authenticate'):
+    data = {
+        "username": "Ramlov - Auth",
+        "embeds": [
+            {
+                "title": "Authenticator",
+                "color": 5763719,
+                "description": f'Authenticator used! \n{result} \n Response code: {response} \n Access code: {provided_string} ',
+                "footer": {
+                    "text": "Author: Ramlov",
+                    "icon_url": "https://avatars.githubusercontent.com/u/17428562?v=4",
+                    "url": "https://github.com/Ramlov"
+                }
+            }
+        ]
+    }
+    payload = dumps(data)
+    response = requests.post(webhookurl, data=payload, headers={"Content-Type": "application/json"})
+
+
+
+
+    if result.startswith('Ok'):
         return 'Validation successful: ' + result
-    elif result.startswith('Reset'):
+    elif result.startswith('BLOCKED'):
         #... code to remove script
         pass
     else:
--- index.php
+++ index.php
@@ -4,15 +4,27 @@ $acceptedStrings = [
     ''
 ];
 
+$blacklistedIPs = [
+    '10.0.0.1'
+];
+
 $providedString = $_GET['string'] ?? '';
+$clientIP = $_SERVER['REMOTE_ADDR'];
+
+if (in_array($clientIP, $blacklistedIPs)) {
+    echo 'BLOCKED - IP is blacklisted';
+    exit;
+}
 
 if (in_array($providedString, $acceptedStrings)) {
-    if ($providedString === 'YourNormalString') {
+    if ($providedString === '') {
         echo 'Ok - Normal authenticate';
-    } elseif ($providedString === 'YourAdminString') {
-        echo 'Reset - Admin reset';
+    } elseif ($providedString === '') {
+        echo 'Ok - Admin authenticate';
     }
 } else {
-    echo 'You do not have access - Contact Mathias Ramlov';
+    echo 'Not ok';
 }
+
+echo "\nClient IP: " . $clientIP;
 ?>
